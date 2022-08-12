62bc9c4d86bda88d71ea500c9e62e08c71389a8f
simon-schaefer <simon.k.schaefer@gmail.com>
2022-08-12 10:21:42 +0200

    updated human body mesh demo and documentation

--- balanna/trimesh.py
+++ balanna/trimesh.py
@@ -8,30 +8,68 @@ from typing import Optional
 def show_mesh(
     vertices: np.ndarray,
     faces: np.ndarray,
-    T_WB: np.ndarray = np.eye(4),
+    transform: np.ndarray = np.eye(4),
     name: Optional[str] = None,
     scene: Optional[trimesh.Scene] = None
 ) -> trimesh.Scene:
+    """Create a mesh from vertices and faces and add it to the scene.
+
+    Args:
+        vertices: mesh vertices (N, 3).
+        faces: mesh faces.
+        transform: transform mesh before adding it to scene (4, 4).
+        name: scene node name of mesh.
+        scene: scene to add mesh to, if None a new scene will be created.
+    """
     if scene is None:
         scene = trimesh.Scene()
     human = trimesh.Trimesh(vertices, faces)
     human.visual.face_colors[:, :3] = [224, 120, 120]
-    scene.add_geometry(human, transform=T_WB, node_name=name)
+    scene.add_geometry(human, transform=transform, node_name=name)
     return scene
 
 
-def show_point_cloud(vertices: np.ndarray, scene: Optional[trimesh.Scene] = None) -> trimesh.Scene:
-    if scene is None:
-        scene = trimesh.Scene()
-    pc = trimesh.points.PointCloud(vertices.reshape(-1, 3))
-    scene.add_geometry(pc, transform=np.eye(4), node_name='markers')
-    return scene
+def show_point_cloud(
+    vertices: np.ndarray,
+    transform: np.ndarray = np.eye(4),
+    name: Optional[str] = None,
+    scene: Optional[trimesh.Scene] = None
+) -> trimesh.Scene:
+    """Add vertices as trimesh point cloud to the scene.
 
+    Trimesh expects a flat point cloud of shape (N, 3). Therefore, if the point cloud is not flat, it will be
+    flattened before passing it to trimesh.
 
-def show_axis(transform: np.ndarray, scene: Optional[trimesh.Scene] = None) -> trimesh.Scene:
+    Args:
+        vertices: point cloud to visualize (..., 3).
+        transform: transform point cloud before adding it to scene (4, 4).
+        name: scene node name of point cloud mesh.
+        scene: scene to add point cloud mesh to, if None a new scene will be created.
+    """
     if scene is None:
         scene = trimesh.Scene()
     if transform.shape != (4, 4):
         raise ValueError(f"Expected single transformation matrix, got {transform.shape}")
-    scene.add_geometry(trimesh.creation.axis(transform=transform))
+    pc = trimesh.points.PointCloud(vertices.reshape(-1, 3))
+    scene.add_geometry(pc, transform=transform, node_name=name)
+    return scene
+
+
+def show_axis(
+    transform: np.ndarray,
+    name: Optional[str] = None,
+    scene: Optional[trimesh.Scene] = None
+) -> trimesh.Scene:
+    """Add coordinate axis as trimesh mesh to the scene.
+
+    Args:
+        transform: axis pose as transformation matrix (4, 4).
+        name: scene node name of axis mesh.
+        scene: scene to add axis to, if None a new scene will be created.
+    """
+    if scene is None:
+        scene = trimesh.Scene()
+    if transform.shape != (4, 4):
+        raise ValueError(f"Expected single transformation matrix, got {transform.shape}")
+    scene.add_geometry(trimesh.creation.axis(), transform=transform, node_name=name)
     return scene
--- bin/display_human_body.py
+++ bin/display_human_body.py
@@ -1,101 +1,55 @@
+import argparse
 import numpy as np
 import pickle as pkl
-import argparse
 import torch
 
 from pathlib import Path
-from typing import Dict, Any
-from balanna.trimesh import show_mesh
-from balanna.display_scenes import display_scenes
+from typing import Dict
 from smplx import SMPL
 
-
-SMPL_MODEL_DIR = Path('~/git/smpl/models/').expanduser().as_posix()
-
-VIEW_POINT = np.array([[-0.50392321, -0.18970999, -0.84265742, 11.00678718],
-                       [ 0.15282067, -0.97977334,  0.12918999, -0.0317945 ],
-                       [-0.8501219 , -0.06367364,  0.52272213, -8.85028203],
-                       [ 0.        ,  0.        ,  0.        ,  1.        ]])
+from balanna.trimesh import show_mesh
+from balanna.display_scenes import display_scenes
 
 
-def get_body_model(batch_size: int, dtype=torch.float32):
-    smpl = SMPL(SMPL_MODEL_DIR,
-                batch_size=batch_size,
-                create_transl=False,
-                gender='neutral',
-                dtype=dtype)
-    return smpl
+def get_body_model(path: Path, batch_size: int, dtype=torch.float32):
+    return SMPL(path.as_posix(), batch_size=batch_size, create_transl=False, gender="male", dtype=dtype)
 
 
+def read_human_body_folder(fname: Path, smpl_model_path: Path) -> Dict[str, np.ndarray]:
+    with fname.open("rb") as f:
+        data = pkl.load(f, encoding="latin1")
+    thetas = torch.tensor(data["fullpose"], dtype=torch.float32)
+    betas = torch.tensor(data["betas"], dtype=torch.float32)
+    trans = torch.tensor(data["trans"], dtype=torch.float32)
 
-def read_human_body_folder(dataset_dir: Path) -> Dict[int, np.ndarray]:
-    marker_fnames = sorted([f for f in dataset_dir.expanduser().glob('*.pkl')])
+    smpl = get_body_model(smpl_model_path, batch_size=len(thetas), dtype=torch.float32)
+    smpl_out = smpl(betas=betas[None], global_orient=thetas[:, :3], body_pose=thetas[:, 3:], pose2rot=True)
 
-    human_body_parameters = {}
-
-    for fname in marker_fnames:
-        if not fname.stem.endswith('_stageii'):
-            continue
-        with fname.open('rb') as f:
-            data = pkl.load(f, encoding='latin1')
-
-        frame = {}
-        frame['theta'] = data['fullpose']
-        frame['beta'] = data['betas']
-        frame['trans'] = data['trans']
-
-        timestamp = int(fname.stem.split('_')[0])
-        human_body_parameters[timestamp] = frame
-        
-    return human_body_parameters
-
-
-def prepare_human_bodies_for_trimesh(human_data: Dict[int, Dict[str, np.ndarray]]) -> Dict[str, Any]:
-    batch_size = len(human_data)
-    smpl = get_body_model(batch_size, dtype=torch.float32)
-
-    # collate body parameters
-    thetas = torch.empty(batch_size, 72)
-    betas = torch.empty(batch_size, 10)
-    timestamps = np.empty(batch_size, dtype=np.int64)
-    trans = np.empty((batch_size, 3))
-    for i, (ts, fr) in enumerate(human_data.items()):
-        thetas[i] = torch.from_numpy(fr['theta'])
-        betas[i] = torch.from_numpy(fr['beta'])
-        timestamps[i] = ts
-        trans[i] = fr['trans']
-
-    smpl_out = smpl(betas=betas, global_orient=thetas[:, :3], body_pose=thetas[:, 3:], pose2rot=True)
-
-    return {'vertices': smpl_out.vertices.cpu().numpy(),
-            'joints': smpl_out.joints.cpu().numpy(),
-            'faces': smpl.faces,
-            'timestamps': timestamps,
-            'trans': trans}
+    return {
+        "vertices": smpl_out.vertices.cpu().numpy(),
+        "joints": smpl_out.joints.cpu().numpy(),
+        "faces": smpl.faces,
+        "trans": trans,
+    }
 
 
 def _parse_args():
-    parser = argparse.ArgumentParser(description='Script to visualize a human body sequence in a trimesh scene.')
-    parser.add_argument('-d', '--dir', type=Path, required=True, help='Root directory of dataset')
+    parser = argparse.ArgumentParser(description="Script to visualize a human body sequence in a trimesh scene.")
+    parser.add_argument("fname", type=Path, help="Root file name to visualize")
+    parser.add_argument("--smpl-model", type=Path, required=True, help="Path to smpl model")
+    return parser.parse_args()
 
-    args = parser.parse_args()
-    return args
 
-
-def run_all_human_bodies(opts):
-    human_dict = read_human_body_folder(opts.dir)
-    human_meshes = prepare_human_bodies_for_trimesh(human_dict)
-
-    for i, ts in enumerate(human_meshes['timestamps']):
-        print(f'current timestamp: {ts}')
+def run_all_human_bodies(fname: Path, smpl_model_path: Path):
+    human_meshes = read_human_body_folder(fname, smpl_model_path)
+    for i, vertices in enumerate(human_meshes['vertices']):
         T_WB = np.eye(4)
-        T_WB[:3, 3] = human_meshes['trans'][i]
-        scene = show_mesh(human_meshes['vertices'][i], human_meshes['faces'], T_WB)
-        yield {'human_mesh': scene}
+        T_WB[:3, 3] = human_meshes["trans"][i]
+        scene = show_mesh(vertices, human_meshes["faces"], transform=T_WB)
+        yield {"human_mesh": scene}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = _parse_args()
-    all_scenes = run_all_human_bodies(args)
+    all_scenes = run_all_human_bodies(args.fname, smpl_model_path=args.smpl_model)
     display_scenes(all_scenes)
-
