8da8b4aecb79eac8fb197d367f159130ef010145
Simon Schaefer <simon.k.schaefer@gmail.com>
2023-03-14 10:17:49 +0100

    Real-Time Rendering (#8)
    * rendering in a real-time application with multi-threading

--- balanna/display_scenes.py
+++ balanna/display_scenes.py
@@ -13,8 +13,12 @@ import vedo
 from functools import partial
 from PIL import Image, ImageQt
 from PyQt5 import Qt
-from typing import Any, Dict, Iterable, Optional, Union
+from PyQt5.QtCore import QObject, QThread, pyqtSignal
+from typing import Any, Dict, Iterable, List, Optional, Union
 from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+
+
+__all__ = ['display_scenes', 'display_real_time', 'RealTimeNode']
 
 
 SceneDictType = Dict[str, Any]
@@ -24,11 +28,10 @@ class MainWindow(Qt.QMainWindow):
 
     def __init__(
         self,
-        scene_iterator: Iterable[SceneDictType],
-        fps: float,
+        image_keys: List[str],
+        scene_keys: List[str],
         video_directory: Optional[Union[pathlib.Path, str]] = None,
         horizontal: bool = True,
-        loop: bool = True,
         show_labels: bool = False,
         use_scene_cam: bool = False,
         debug: bool = False,
@@ -40,16 +43,11 @@ class MainWindow(Qt.QMainWindow):
             vl = Qt.QHBoxLayout()
         else:
             vl = Qt.QVBoxLayout()
-        self.scene_iterator = scene_iterator
-        self.fps = fps
         self.show_labels = show_labels
         self.use_scene_cam = use_scene_cam
         self.debug = debug
-        scene_dict = self.get_next_scene_dict()
-        if scene_dict is None:
-            return
 
-        image_keys = [key for key, value in scene_dict.items() if isinstance(value, np.ndarray)]
+        # Set up image-based widgets (i.e. numpy array inputs).
         vl1 = Qt.QHBoxLayout()
         self.image_widge_dict = dict()
         for key in image_keys:
@@ -57,7 +55,7 @@ class MainWindow(Qt.QMainWindow):
             vl1.addWidget(self.image_widge_dict[key])
         vl.addLayout(vl1)
 
-        scene_keys = [key for key, value in scene_dict.items() if isinstance(value, trimesh.Scene)]
+        # Set up 3D scene widgets (i.e. trimesh scenes).
         self.scene_key_dict = {key: i for i, key in enumerate(scene_keys)}
         self.vps = []
         self.vtkWidgets = []
@@ -76,41 +74,29 @@ class MainWindow(Qt.QMainWindow):
             vp.add_callback('MiddleButtonRelease', self.__on_mouse_end)
             vp.add_callback('MouseWheelForward', partial(self.__align_event, align_id=scene_id))
             vp.add_callback('MouseWheelBackward', partial(self.__align_event, align_id=scene_id))
-            vp.add_callback('KeyPress', self.__on_key)
+            vp.add_callback('KeyPress', self._on_key)
             vp.show()
             self.vtkWidgets.append(vtk_widget)
             self.vps.append(vp)
         vl.addLayout(vl2)
 
+        # Set up mouse timer for synchronizing mouse interactions across multiple (3D) widgets.
+        # Therefore, the interaction is detected, tracked and transferred to all other 3D widgets.
         self._mouse_timer = Qt.QTimer(self)
-        self._mouse_timer.timeout.connect(self.__align_event)
+        self._mouse_timer.timeout.connect(self.__align_event)  # noqa
         self._current_align_id = None
 
         frame.setLayout(vl)
         self.setCentralWidget(frame)
         self.show()
 
+        # Set up video directory for storing screenshots.
         self.video_directory = None
         self.__video_index = 0
         if video_directory is not None:
             self.video_directory = pathlib.Path(video_directory)
             if self.video_directory.exists():
                 print("\033[93m" + "Video directory already exists, overwriting it ..." + "\033[0m")
-
-        self.render_(scene_dict, resetcam=True)
-        self.timer = Qt.QTimer(self)
-        self.timer.timeout.connect(self.render_next_scene)
-        if self.debug:
-            print("\033[36m" + "Setup ready, starting to loop ..." + "\033[0m")
-        if loop:
-            self.toggle_looping()
-
-    def render_next_scene(self, resetcam: bool = False) -> None:
-        scene_dict = self.get_next_scene_dict()
-        if scene_dict is not None:
-            self.render_(scene_dict, resetcam=resetcam)
-        elif self.timer.isActive():
-            self.timer.stop()
 
     def render_(self, scene_dict: SceneDictType, resetcam: bool = False):
         start_time = time.perf_counter()
@@ -203,13 +189,9 @@ class MainWindow(Qt.QMainWindow):
             self.vps[k].renderer.SetActiveCamera(camera)
             self.vps[k].render()
 
-    def __on_key(self, event_dict) -> None:
+    def _on_key(self, event_dict) -> None:
         key_pressed = event_dict["keyPressed"]
-        if key_pressed == "s":
-            self.toggle_looping()
-        elif key_pressed == "n":
-            self.render_next_scene()
-        elif key_pressed == "z":
+        if key_pressed == "z":
             if self.num_scenes > 0:
                 self.vps[0].render(resetcam=True)
                 self.__align_event(align_id=0)
@@ -218,12 +200,7 @@ class MainWindow(Qt.QMainWindow):
             prefix = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S_%f")
             self.screenshot(screenshot_dir, prefix=prefix)
         elif key_pressed == "h":
-            print("Usage:",
-                  "\n\tq: quit",
-                  "\n\ts: play / pause",
-                  "\n\tn: next",
-                  "\n\tz: reset view",
-                  "\n\tk: take screenshot")
+            self.print_usage()
         elif key_pressed == "q":
             self.close()
 
@@ -236,17 +213,12 @@ class MainWindow(Qt.QMainWindow):
             self._mouse_timer.stop()
         self._current_align_id = None
 
-    def get_next_scene_dict(self) -> Optional[SceneDictType]:
-        try:
-            return next(self.scene_iterator)  # noqa
-        except StopIteration:
-            return None
-
-    def toggle_looping(self):
-        if self.timer.isActive():
-            self.timer.stop()
-        else:
-            self.timer.start(int(1 / self.fps * 1000))  # in milliseconds
+    @staticmethod
+    def print_usage():
+        print("Usage:",
+              "\n\tq: quit",
+              "\n\tz: reset view",
+              "\n\tk: take screenshot")
 
     def screenshot(self, directory: pathlib.Path, prefix: str):
         image_paths = []
@@ -287,6 +259,167 @@ class MainWindow(Qt.QMainWindow):
         return len(self.vps)
 
 
+class MainWindowDataset(MainWindow):
+
+    def __init__(
+        self,
+        scene_iterator: Iterable[SceneDictType],
+        fps: float,
+        video_directory: Optional[Union[pathlib.Path, str]] = None,
+        horizontal: bool = True,
+        loop: bool = True,
+        show_labels: bool = False,
+        use_scene_cam: bool = False,
+        debug: bool = False,
+        parent: Qt.QWidget = None
+    ):
+        self.scene_iterator = scene_iterator
+        self.fps = fps
+        scene_dict = self.get_next_scene_dict()
+        if scene_dict is None:
+            return
+
+        image_keys = [key for key, value in scene_dict.items() if isinstance(value, np.ndarray)]
+        scene_keys = [key for key, value in scene_dict.items() if isinstance(value, trimesh.Scene)]
+        super(MainWindowDataset, self).__init__(
+            image_keys=image_keys,
+            scene_keys=scene_keys,
+            video_directory=video_directory,
+            horizontal=horizontal,
+            show_labels=show_labels,
+            use_scene_cam=use_scene_cam,
+            debug=debug,
+            parent=parent
+        )
+
+        self.render_(scene_dict, resetcam=True)
+
+        # Setup looping timer and connect it to render a new scene at every timer callback.
+        # If loop is true, start looping right away.
+        self.timer = Qt.QTimer(self)
+        self.timer.timeout.connect(self.render_next_scene)  # noqa
+        if loop:
+            self.toggle_looping()
+
+    def render_next_scene(self, resetcam: bool = False) -> None:
+        scene_dict = self.get_next_scene_dict()
+        if scene_dict is not None:
+            self.render_(scene_dict, resetcam=resetcam)
+        elif self.timer.isActive():
+            self.timer.stop()
+
+    def _on_key(self, event_dict) -> None:
+        super(MainWindowDataset, self)._on_key(event_dict)
+        key_pressed = event_dict["keyPressed"]
+        if key_pressed == "s":
+            self.toggle_looping()
+        elif key_pressed == "n":
+            self.render_next_scene()
+
+    @staticmethod
+    def print_usage():
+        MainWindow.print_usage()
+        print("\ts: play / pause",
+              "\n\tn: next")
+
+    def get_next_scene_dict(self) -> Optional[SceneDictType]:
+        try:
+            return next(self.scene_iterator)  # noqa
+        except StopIteration:
+            return None
+
+    def toggle_looping(self):
+        if self.timer.isActive():
+            self.timer.stop()
+        else:
+            self.timer.start(int(1 / self.fps * 1000))  # in milliseconds
+
+
+class RealTimeNode(QObject):
+    """Template class for real time rendering.
+
+    Examples:
+        class DemoNode(RealTimeNode):
+
+            def callback(self):
+                scene = trimesh.Scene()
+                box_mesh = trimesh.creation.box(bounds=[(-1, -1, 0), (1, 1, 1)])
+                scene.add_geometry(box_mesh)
+                self.render({"scene": scene})
+
+                if condition:
+                    self.close()
+    """
+    finished = pyqtSignal()
+    running = pyqtSignal()
+    scene_dict_emitter = pyqtSignal(dict)
+
+    def callback(self):
+        raise NotImplementedError
+
+    def run(self):
+        while True:
+            if not self.running:
+                continue
+            self.callback()
+
+    def render(self, scene_dict: SceneDictType):
+        self.scene_dict_emitter.emit(scene_dict)  # noqa
+
+    def close(self):
+        self.finished.emit()  # noqa
+
+
+class MainWindowRealTime(MainWindow):
+
+    def __init__(
+        self,
+        worker: QObject,
+        image_keys: List[str],
+        scene_keys: List[str],
+        video_directory: Optional[Union[pathlib.Path, str]] = None,
+        horizontal: bool = True,
+        show_labels: bool = False,
+        use_scene_cam: bool = False,
+        debug: bool = False,
+        parent: Qt.QWidget = None
+    ):
+        super(MainWindowRealTime, self).__init__(
+            image_keys=image_keys,
+            scene_keys=scene_keys,
+            video_directory=video_directory,
+            horizontal=horizontal,
+            show_labels=show_labels,
+            use_scene_cam=use_scene_cam,
+            debug=debug,
+            parent=parent
+        )
+
+        # Set up multi-processing pipeline.
+        self.thread = QThread()
+        self.worker = worker
+        self.worker.moveToThread(self.thread)
+        self.thread.started.connect(self.worker.run)
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        self.worker.scene_dict_emitter.connect(self.render_)
+        self.thread.start()
+
+        self.show()
+
+    def _on_key(self, event_dict) -> None:
+        super(MainWindowRealTime, self)._on_key(event_dict)
+        key_pressed = event_dict["keyPressed"]
+        if key_pressed == "s":
+            self.worker.running = not self.worker.running
+
+    @staticmethod
+    def print_usage():
+        MainWindow.print_usage()
+        print("\ts: play / pause")
+
+
 def display_scenes(
     scene_iterator: Iterable[SceneDictType],
     horizontal: bool = True,
@@ -313,7 +446,7 @@ def display_scenes(
         debug: printing debug information.
     """
     app = Qt.QApplication([])
-    window = MainWindow(
+    window = MainWindowDataset(
         scene_iterator=scene_iterator,
         fps=fps,
         video_directory=video_directory,
@@ -322,6 +455,56 @@ def display_scenes(
         show_labels=show_labels,
         debug=debug,
         use_scene_cam=use_scene_cam
+    )
+    app.aboutToQuit.connect(window.on_close)
+    app.exec_()
+
+
+def display_real_time(
+    worker: RealTimeNode,
+    image_keys: Optional[List[str]] = None,
+    scene_keys: Optional[List[str]] = None,
+    horizontal: bool = True,
+    video_directory: Optional[pathlib.Path] = None,
+    show_labels: bool = False,
+    use_scene_cam: bool = False,
+    debug: bool = False
+):
+    """Display scenes from a real-time application, i.e. render in-coming scenes directly.
+
+    The worker is a process that generates each new scene in a separate thread and passes it to the renderer.
+    For a smooth rendering process and for simplification, the image and scene keys have to be known in advances
+    and cannot be changed afterwards.
+
+    Args:
+        worker: generator of new scenes.
+        image_keys: names of images in rendered dictionary.
+        scene_keys: names of 3D scenes in rendered dictionary.
+        horizontal: window orientation, horizontal or vertical stacking.
+        video_directory: directory for storing screenshots.
+        show_labels: display the scene dict keys.
+        use_scene_cam: use camera transform from trimesh scenes.
+        debug: printing debug information.
+    """
+    if image_keys is None and scene_keys is None:
+        raise ValueError("Neither image nor scene keys, provide at least one key!")
+    if not hasattr(worker, 'run'):
+        raise ValueError("Worker must have method run()")
+    if image_keys is None:
+        image_keys = []
+    if scene_keys is None:
+        scene_keys = []
+
+    app = Qt.QApplication([])
+    window = MainWindowRealTime(
+        worker=worker,
+        image_keys=image_keys,
+        scene_keys=scene_keys,
+        video_directory=video_directory,
+        horizontal=horizontal,
+        show_labels=show_labels,
+        use_scene_cam=use_scene_cam,
+        debug=debug
     )
     app.aboutToQuit.connect(window.on_close)
     app.exec_()
