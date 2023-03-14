2533d78ae0cb8446b7e1e49f6982b2209deeb245
Simon Schaefer <simon.k.schaefer@gmail.com>
2023-03-14 10:00:55 +0100

    Feature/realtime renderer (#7)
    * rendering in a real-time application with multi-threading

--- balanna/__init__.py
+++ balanna/__init__.py
@@ -2,4 +2,4 @@ import balanna.camera_trajectories
 import balanna.utils
 import balanna.trimesh
 
-from balanna.display_scenes import display_scenes
+from balanna.display_scenes import display_scenes, display_real_time, RealTimeNode
