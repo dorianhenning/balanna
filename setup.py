ec33654b5e91fe33ddfe59d5492de7973cbb5112
simon-schaefer <simon.k.schaefer@gmail.com>
2023-05-22 13:07:07 +0200

    v1.1 -> 1.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ REQUIRED = [
 
 setup(
     name='balanna',
-    version='1.1',
+    version='1.2',
     description='Visualization Tools for 2D & 3D stuff working out-of-the-box',
     author='Simon Schaefer & Dorian Henning',
     author_email='simon.k.schaefer@gmail.com',
