45d965e4f5f77b8200b7b3031eadc88ec98ed34b
simon-schaefer <simon.k.schaefer@gmail.com>
2023-09-25 09:11:28 +0200

    removed expanded code for simplicity of the library
    - realtime app
    - off-screen rendering
    
    => focus on the basics, off-screen rendering anyway better suited in tools like Paraview

--- balanna/__init__.py
+++ balanna/__init__.py
@@ -2,10 +2,8 @@ import balanna.camera_trajectories as camera_trajectories
 import balanna.utils as utils
 import balanna.trimesh as trimesh
 
-from balanna.rendering_dataset import render_dataset
 from balanna.window_dataset import display_dataset, display_dataset_custom, MainWindowDataset
 from balanna.window_generator import display_generated, display_scenes, display_generated_custom, MainWindowGenerator
-from balanna.window_real_time import display_real_time, RealTimeNode
 from balanna.window_base import SceneDictType
 
 
@@ -17,10 +15,7 @@ __all__ = [
   "display_scenes",
   "display_generated",
   "display_generated_custom",
-  "display_real_time", 
-  "RealTimeNode",
   "MainWindowDataset",
   "MainWindowGenerator",
-  "render_dataset",
   "SceneDictType",
 ]
