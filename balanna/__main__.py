1ef73da6ae43722d59c4e2c6def9e1a414bf7f4d
simon-schaefer <simon.k.schaefer@gmail.com>
2024-04-12 16:03:16 +0200

    heatmap3d visualization mode

--- balanna/__main__.py
+++ balanna/__main__.py
@@ -2,19 +2,18 @@ import argparse
 import numpy as np
 import pathlib
 import pickle as pkl
-import tqdm
 import sys
 
 from loguru import logger
 
 from balanna.json_parser import load_scene_from_json
-from balanna.trimesh import show_point_cloud
+from balanna.trimesh import show_axis, show_point_cloud
 from balanna.window_dataset import display_dataset
 
 
 def _parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("mode", choices=["pointcloud", "scenes", "json"])
+    parser.add_argument("mode", choices=["pointcloud", "heatmap3d", "scenes", "json"])
     parser.add_argument("directory", type=pathlib.Path, help="Data directory or file")
     parser.add_argument("--fps", type=int, help="displaying fps", default=10)
     parser.add_argument("--use-scene-cam", action="store_true", help="use scene camera")
@@ -32,7 +31,7 @@ def main(args):
     else:
         if args.mode == "json":
             suffix = ".json"
-        elif args.mode == "pointcloud":
+        elif args.mode in ["pointcloud", "heatmap3d"]:
             suffix = ".txt"
         else:
             suffix = ".pkl"
@@ -43,9 +42,25 @@ def main(args):
     # Load and process files into scenes.
     scenes = []
     if args.mode == "pointcloud":
-        for k, file in enumerate(files):
+        for file in files:
             point_cloud = np.loadtxt(file).reshape(-1, 6)
             scene = show_point_cloud(point_cloud[:, :3], colors=point_cloud[:, 3:])
+            scenes.append({'scene': scene})
+
+    elif args.mode == "heatmap3d":
+        from matplotlib import colormaps
+        import matplotlib.colors as mcolors
+        for file in files:
+            logger.debug(f"Loading file {file}")
+            heatmap3d = np.loadtxt(file).reshape(-1, 4)
+
+            values = heatmap3d[:, 3]
+            norm = mcolors.Normalize(vmin=min(values), vmax=max(values))
+            cmap = colormaps['jet']
+            colors = np.array([cmap(norm(value))[:3] for value in values])
+            colors = (colors * 255).astype(np.uint8)
+
+            scene = show_point_cloud(heatmap3d[:, :3], colors=colors)
             scenes.append({'scene': scene})
 
     elif args.mode == "scenes":
@@ -53,10 +68,12 @@ def main(args):
             with open(file, 'rb') as f:
                 scene_dict = pkl.load(f)
             scenes.append(scene_dict)
+
     elif args.mode == "json":
         for k, file in enumerate(files):
             scene_dict = load_scene_from_json(file)
             scenes.append(scene_dict)
+
     else:
         raise ValueError(f"Invalid displaying mode {args.mode}")
 
